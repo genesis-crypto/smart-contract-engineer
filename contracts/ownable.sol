97f020884d02f628b67e38201c8b774468c09ea9
Pedro <p-cardozo@hotmail.com>
2023-01-17 23:27:36 -0300

    feat(smart-contract): including Ownable solidity contract

---
+++ contracts/ownable.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.17;
+
+contract Ownable {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "not owner");
+        _;
+    }
+
+    function setOwner(address _newOwner) external onlyOwner {
+        require(_newOwner != address(0), "new owner = zero address");
+        owner = _newOwner;
+    }
+}
