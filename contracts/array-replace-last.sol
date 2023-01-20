30b32fa34f624b3dcee0226239b4428d0c4ff9b1
Pedro <p-cardozo@hotmail.com>
2023-01-20 00:00:21 -0300

    feat(smart-contract): including ArrayReplaceLast solidity contract

---
+++ contracts/array-replace-last.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.17;
+
+contract ArrayReplaceLast {
+    uint[] public arr = [1, 2, 3, 4];
+
+    function remove(uint _index) external {
+        require(_index < arr.length, "out of bound");
+
+        delete arr[_index];
+        arr[_index] = arr[arr.length - 1];
+        arr.pop();
+    }
+}
